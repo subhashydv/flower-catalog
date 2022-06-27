9a9323d7d8c4b0ab3cce371b9555818fd148ec9f
Subhash Yadav <99490164+subhashydv@users.noreply.github.com>
2022-06-27 18:15:46 +0530

    Added file for storing comments

--- src/requestHandler.js
+++ src/requestHandler.js
@@ -34,40 +34,60 @@ const getTimeStamp = () => {
 };
 
 const spaceParse = text => {
-  return text.replace('+', ' ');
+  return text;
 }
 
 const generateHtml = reviews => {
   return reviews.map(review => {
     const name = spaceParse(review.name);
     const comment = spaceParse(review.comment);
-    return `<td>${review.timeStamp}</td><td>${name}</td><td>${comment}</td>`;
+    return `<tr><td>${review.timeStamp}</td><td>${name}</td><td>${comment}</td></tr>`;
   }).join('');
-}
+};
 
 const getHtml = (reviews) => {
   const content = fs.readFileSync('./public/guestbook.html', 'utf-8');
   const table = generateHtml(reviews);
   return content.replace('__BODY__', table);
+};
+
+const storeReviews = reviews => {
+  fs.writeFileSync('reviews.json', JSON.stringify(reviews), 'utf8');
+};
+
+const readPrevReviews = () => {
+  return JSON.parse(fs.readFileSync('reviews.json', 'utf-8'));
 }
 
-const guestBookHandler = (request, response) => {
-  const reviews = [];
+const guestBookHandler = (request, response, reviews) => {
   const { name, comment } = request;
   const timeStamp = getTimeStamp();
   reviews.push({ name, comment, timeStamp });
+  storeReviews(reviews);
+  const html = getHtml(reviews);
+  response.send(html);
+  return true;
+};
+
+const showReviews = (request, response, reviews) => {
   const html = getHtml(reviews);
   response.send(html);
   return true;
 };
 
 const requestHandler = (request, response, serveFrom) => {
-  const { uri } = request;
+  const { uri, name, comment } = request;
   if (uri.includes('.') || uri === '/') {
     return serveFileContent(request, response, serveFrom);
   }
+  if (uri === '/guestbook' && name && comment) {
+    const reviews = readPrevReviews();
+    return guestBookHandler(request, response, reviews);
+  }
+
   if (uri === '/guestbook') {
-    return guestBookHandler(request, response);
+    const reviews = readPrevReviews();
+    return showReviews(request, response, reviews);
   }
   return false
 }
